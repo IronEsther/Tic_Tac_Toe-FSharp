2f589ef7a8347fd719080d281b85d4aa9c343b1b
gummyflummy <D.Zumstein.Inf21@stud.bbbaden.ch>
2023-09-14 08:30:43 +0200

    Code updated
    Code Verison geändert und Beschreibungen hinzugefügt

--- Program/TicTacToe-Fsharp/Program.fs
+++ Program/TicTacToe-Fsharp/Program.fs
@@ -1,5 +1,4 @@
 ﻿
-
 type Player = 
     | None
     | X
@@ -12,7 +11,9 @@ let emptyBoard =
       [| None; None; None |];
       [| None; None; None |]|]
 
+// Zeigt das aktuelle Spielfeld an
 let printBoard (board: Board) =
+    printfn "Aktuelles Spielfeld:\n"
     for row in board do
         for cell in row do
             match cell with
@@ -21,6 +22,7 @@ let printBoard (board: Board) =
             | O -> printf "O "
         printfn ""
 
+// Überprüft, ob der Spieler gewonnen hat
 let hasWon (player: Player) (board: Board) =
     let allSame (line: Player array) = Array.forall (fun cell -> cell = player) line
     let anyRow = board |> Array.exists allSame
@@ -29,45 +31,48 @@ let hasWon (player: Player) (board: Board) =
     let diagonal2 = [| board.[0].[2]; board.[1].[1]; board.[2].[0] |]
     anyRow || anyColumn || allSame diagonal1 || allSame diagonal2
 
+// Wechselt den Spieler
 let togglePlayer player =
     match player with
     | X -> O
     | O -> X
     | None -> None
 
+// Überprüft, ob ein Zug gültig ist
 let isValidMove (board:Board) row col =
     row >= 0 && row <= 2 && col >= 0 && col <= 2 && board.[row].[col] = None
 
+// Eingabeaufforderung für den Spieler
 let rec getInput prompt =
     printf "%s" prompt
     match System.Int32.TryParse(System.Console.ReadLine()) with
     | (true, value) when value >= 0 && value <= 2 -> value
     | _ -> 
-        printfn "Gib eine Zahl zwischen 0 und 2 ein"
+        printfn "Ungültige Eingabe! Bitte gib eine Zahl zwischen 0 und 2 ein."
         getInput prompt
 
-
+// Hauptspiellogik
 let rec playGame board currentPlayer =
     printBoard board
     printfn "%A ist dran!" currentPlayer
     let row = getInput "Wähle Zeile (0-2): "
-    let col = getInput "Wähle Reihe (0-2): "
+    let col = getInput "Wähle Spalte (0-2): "
 
     if isValidMove board row col then
         board.[row].[col] <- currentPlayer
         if hasWon currentPlayer board then
-            printfn "%A hat gewonnen!" currentPlayer
+            printfn "\nHerzlichen Glückwunsch! %A hat gewonnen!" currentPlayer
         else if Array.forall (fun row -> Array.forall (fun cell -> cell <> None) row) board then
-            printfn "unentschieden!"
+            printfn "\nDas Spiel endet unentschieden!"
         else
             playGame board (togglePlayer currentPlayer)
     else
-        printfn "Du Rabauke, das ist nicht erlaubt"
+        printfn "\nDu kleiner Rabauke, das ist nicht erlaubt."
         playGame board currentPlayer
 
 [<EntryPoint>]
 let main argv =
+    printfn "Willkommen bei Tic Tac Toe!\n"
     playGame emptyBoard X
     0
 
-    
